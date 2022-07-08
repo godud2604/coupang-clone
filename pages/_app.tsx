387cf581660e5750eb67c2415d8776c76c5e131c
godud2604 <ees238@naver.com>
2022-07-09 00:06:45 +0900

    Delete globals, home css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { useState } from "react";
-import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { Global, ThemeProvider } from '@emotion/react';
+import type { AppProps } from 'next/app';
+import { useState } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+
+import global from 'src/styles/global';
+import Layout from 'src/styles/layout';
+import theme from 'src/styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
@@ -9,7 +13,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Global styles={global} />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
       </Hydrate>
     </QueryClientProvider>
   );
